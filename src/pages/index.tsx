054014924ea47dc13011f4d39e8dc4cbdc7414f2
Stephan Loh <sloh4137@gmail.com>
2020-10-20 18:35:23 -0400

    Fix sign in page unmount

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
 import React from "react"
-import { Link, PageProps } from "gatsby"
+import { PageProps } from "gatsby"
 import ReactGlobe from "react-globe"
 
 import SEO from "components/seo"
-import SignIn from "components/SignIn"
 
 const IndexPage = ({}: PageProps) => {
     return (
         <>
             <SEO title="Home" />
             <ReactGlobe height="10vh" globeBackgroundTexture={null} />
-            <SignIn />
         </>
     )
 }
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,14 +1,58 @@
 import React from "react"
 import { PageProps } from "gatsby"
+import firebase from "gatsby-plugin-firebase"
+import {
+    Button,
+    Container,
+    Grid,
+    Typography,
+    makeStyles,
+} from "@material-ui/core"
 
 import SEO from "components/seo"
-import SignIn from "components/SignIn"
+import ClientOnly from "components/ClientOnly"
+import useIsSignedIn from "hooks/useIsSignedIn"
+import { FirebaseAuth } from "components/SignIn"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: theme.spacing(1),
+    },
+}))
 
 const SignInPage = ({}: PageProps) => {
+    const classes = useStyles()
+    const isSignedIn = useIsSignedIn()
     return (
         <>
             <SEO title="Sign In" />
-            <SignIn />
+            <Container maxWidth="md" className={classes.root}>
+                <Grid
+                    container
+                    alignItems="center"
+                    justify="center"
+                    direction="column"
+                >
+                    <ClientOnly>
+                        {isSignedIn ? (
+                            <>
+                                <Typography align="center">{`Welcome ${
+                                    firebase.auth().currentUser?.displayName
+                                }`}</Typography>
+                                <Button
+                                    onClick={() => firebase.auth().signOut()}
+                                >
+                                    Sign out
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <FirebaseAuth />
+                            </>
+                        )}
+                    </ClientOnly>
+                </Grid>
+            </Container>
         </>
     )
 }
