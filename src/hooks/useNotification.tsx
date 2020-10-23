450de866954fd1a3ad7ccf348ecd52e06cdf6ab5
Stephan Loh <sloh4137@gmail.com>
2020-10-22 20:51:50 -0400

    Add hooks notification but it definitely won't work. Just commiting for that version history

---
+++ src/hooks/useNotification.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import { Notification, NotificationProps } from "components/Notification"
+
+export default function useNotification(props: Omit<NotificationProps, "open" | "handleClose">) {
+
+}
