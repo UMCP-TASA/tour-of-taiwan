f5495ad4c64044383b3ef5dec385bb79c2380928
Stephan Loh <sloh4137@gmail.com>
2020-10-23 15:35:56 -0400

    Update provider to handle notifications

--- src/components/Notification/Provider.tsx
+++ src/components/Notification/Provider.tsx
@@ -4,13 +4,11 @@ import { Notification, NotificationProps } from "."
 type Options = Omit<NotificationProps, "open" | "handleClose">
 
 export type NotificationContextType = {
-    notification: JSX.Element
     setNotification: (options: Options) => void
 }
 
 export const NotificationContext = React.createContext<NotificationContextType>(
     {
-        notification: <></>,
         setNotification: __ => {},
     }
 )
@@ -21,46 +19,33 @@ type Props = {
 
 // Currently only one notification can be shown at a time so oops
 const Provider = ({ children }: Props) => {
-    const [rawNotification, setRawNotification] = React.useState(<></>)
     const [open, setOpen] = React.useState(false)
     const [props, setProps] = React.useState<Options>({
-        // open: open,
-        // handleClose: () => setOpen(false),
         message: "",
         severity: "info",
     })
 
     const contextValue = React.useMemo(() => {
-        console.log("Context being set")
         function setNotification(options: Options) {
-            // setRawNotification(
-            //     <Notification
-            //         open={open}
-            //         handleClose={() => setOpen(false)}
-            //         {...options}
-            //     />
-            // )
             setProps({
-                ...props,
                 ...options,
             })
             setOpen(true)
         }
 
         return {
-            notification: rawNotification,
             setNotification,
         }
     }, [setProps, setOpen])
 
-    console.log(props)
-
     return (
         <NotificationContext.Provider value={contextValue}>
             {children}
-            <div id="notif">
-                <Notification open={open} handleClose={() => setOpen(false)} {...props} />
-            </div>
+            <Notification
+                open={open}
+                handleClose={() => setOpen(false)}
+                {...props}
+            />
         </NotificationContext.Provider>
     )
 }
