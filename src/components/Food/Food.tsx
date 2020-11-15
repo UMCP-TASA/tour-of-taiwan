5b4a95003a58b01f73326b2a206c29907a99fb8b
Angela Pan <a.pan2596@gmail.com>
2020-11-14 21:49:47 -0500

    npm run format

--- src/components/Food/Food.tsx
+++ src/components/Food/Food.tsx
@@ -1,70 +1,89 @@
 import React from "react"
 import { FoodFragment } from "graphql-types"
-import CardContent from '@material-ui/core/CardContent'; 
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import CardContent from "@material-ui/core/CardContent"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
 import { Grid, makeStyles, Card, Paper } from "@material-ui/core"
 
-type Props = { 
+type Props = {
     food: FoodFragment | null | undefined
 }
 
-export default function Food({food} : Props) {
-
+export default function Food({ food }: Props) {
     if (!food) {
         throw new Error("City does not exist")
     }
 
     if (!food.frontmatter) {
-        throw new Error("Frontmatter does not exist");
+        throw new Error("Frontmatter does not exist")
     }
 
     if (!food.frontmatter.imgsrc) {
-        throw new Error("Frontmatter does not exist");
+        throw new Error("Frontmatter does not exist")
     }
 
     if (!food.frontmatter.video) {
-        throw new Error("Frontmatter does not exist");
+        throw new Error("Frontmatter does not exist")
     }
 
     function getEmbedUrl(url: string) {
-        var reg = /(?:http?s?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g;
-        var match = reg.exec(url);
+        var reg = /(?:http?s?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g
+        var match = reg.exec(url)
         if (match != null) {
-            return 'https://www.youtube.com/embed/' + match[1];
+            return "https://www.youtube.com/embed/" + match[1]
         }
-        return 'https://www.youtube.com/embed/A9fdHs1uxGo' //random taiwan vid that should never show up
+        return "https://www.youtube.com/embed/A9fdHs1uxGo" //random taiwan vid that should never show up
     }
-        
+
     return (
         <Grid
             container
             direction="row"
             justify="center"
             alignItems="stretch"
-            style={{height: '80vh'}}
+            style={{ height: "80vh" }}
         >
             <Grid item xs={6}>
-                <img src={food.frontmatter.imgsrc} style={{width: '100%', height: '100%', objectFit: 'cover', borderRadius: '20px', padding: '10px'}}/>
+                <img
+                    src={food.frontmatter.imgsrc}
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        borderRadius: "20px",
+                        padding: "10px",
+                    }}
+                />
             </Grid>
-            <Grid item xs={6} style={{padding: '10px'}}> 
-                <Typography gutterBottom variant="h3" component="h2" align='center' color='textPrimary' style={{fontWeight: 'bold', padding: '10px 0px'}}>
+            <Grid item xs={6} style={{ padding: "10px" }}>
+                <Typography
+                    gutterBottom
+                    variant="h3"
+                    component="h2"
+                    align="center"
+                    color="textPrimary"
+                    style={{ fontWeight: "bold", padding: "10px 0px" }}
+                >
                     {food.frontmatter.name}
                 </Typography>
-                <iframe src={getEmbedUrl(food.frontmatter.video)} 
+                <iframe
+                    src={getEmbedUrl(food.frontmatter.video)}
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                     frameBorder="0"
                     allowFullScreen
-                    style={{borderRadius: '7px', width: '100%', height: '35vh'}}
+                    style={{
+                        borderRadius: "7px",
+                        width: "100%",
+                        height: "35vh",
+                    }}
                 />
-                                <div dangerouslySetInnerHTML={{__html: food.html}} />
-
+                <div dangerouslySetInnerHTML={{ __html: food.html }} />
             </Grid>
         </Grid>
     )
-//     <Grid item>
-//     <Typography gutterBottom variant="h5" component="h2" align='center' color='textPrimary'>
-//         Swipe To Explore!
-//     </Typography>
-// </Grid>
+    //     <Grid item>
+    //     <Typography gutterBottom variant="h5" component="h2" align='center' color='textPrimary'>
+    //         Swipe To Explore!
+    //     </Typography>
+    // </Grid>
 }
--- src/pages/food.tsx
+++ src/pages/food.tsx
@@ -3,17 +3,16 @@ import { PageProps, graphql } from "gatsby"
 import { Grid, makeStyles, Card, Paper } from "@material-ui/core"
 import { FoodPageQuery, FoodFragment } from "graphql-types"
 import { Food } from "components/Food"
-import SwipeableViews from 'react-swipeable-views';
+import SwipeableViews from "react-swipeable-views"
 import SEO from "components/seo"
-
 
 const useStyles = makeStyles(theme => ({
     container: {
         height: "100%",
         width: "100%",
         position: "absolute",
-        top: '0',
-        backgroundImage: 'url(/assets/foodBackground.svg)',
+        top: "0",
+        backgroundImage: "url(/assets/foodBackground.svg)",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
     },
@@ -21,18 +20,26 @@ const useStyles = makeStyles(theme => ({
 
 const FoodPage = ({ data }: PageProps<FoodPageQuery>) => {
     const classes = useStyles()
-    const food_lst = [
-        {data: data.tanghulu},
-        {data: data.tanghulu}
-    ];
+    const food_lst = [{ data: data.tanghulu }, { data: data.tanghulu }]
     return (
         <>
             <SEO title="Food" />
             <div className={classes.container}>
-                <div style={{width: '80%',height: '80vh', backgroundColor: '#fff', boxShadow: 'rgba(0, 0, 0, .2) 0px 0px 5px 2px', marginLeft: '50%', marginTop: '7%', transform: 'translate(-50%)', borderRadius: '20px'}}>
+                <div
+                    style={{
+                        width: "80%",
+                        height: "80vh",
+                        backgroundColor: "#fff",
+                        boxShadow: "rgba(0, 0, 0, .2) 0px 0px 5px 2px",
+                        marginLeft: "50%",
+                        marginTop: "7%",
+                        transform: "translate(-50%)",
+                        borderRadius: "20px",
+                    }}
+                >
                     <SwipeableViews enableMouseEvents>
-                        {food_lst.map(({data}) => (
-                            <Food food={data}/>
+                        {food_lst.map(({ data }) => (
+                            <Food food={data} />
                         ))}
                     </SwipeableViews>
                 </div>
@@ -44,18 +51,20 @@ const FoodPage = ({ data }: PageProps<FoodPageQuery>) => {
 export default FoodPage
 
 export const query = graphql`
-fragment Food on MarkdownRemark {
-    id
-    frontmatter {
-      name
-      imgsrc
-      video
+    fragment Food on MarkdownRemark {
+        id
+        frontmatter {
+            name
+            imgsrc
+            video
+        }
+        html
     }
-    html
-  }
     query FoodPage {
-        tanghulu: markdownRemark(frontmatter: {name: {eq: "Tanghulu"}, category: {eq: "food"}}) {
+        tanghulu: markdownRemark(
+            frontmatter: { name: { eq: "Tanghulu" }, category: { eq: "food" } }
+        ) {
             ...Food
-          }
+        }
     }
 `
